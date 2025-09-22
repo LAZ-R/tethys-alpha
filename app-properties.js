e5c0ecbc5fe1edd3d545e6565a422f30afe15129
LAZ-R <crazydrawer62@gmail.com>
2025-09-22 18:19:36 +0200

    v 0.0.3

---
+++ app-properties.js
@@ -0,0 +1,6 @@
+export const APP_NAME = 'TÉTHYS (alpha)';
+export const APP_VERSION = '0.0.3';
+export const APP_LOCAL_STORAGE_ID = `tethys-alpha-003`;
+
+// CHANGELOG
+// v 0.0.3 - switch to ALPHA env
